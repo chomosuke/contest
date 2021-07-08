ac88b9503e97c6cd493229b57ae17c4e6de8ccc2
chomosuke <chomosuke@users.noreplay.github.com>
2021-07-09 00:56:56 +1200

    min_distance done

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,5 @@
 mod min_distance;
 
 fn main() {
-    println!("{:?}", min_distance::min_distance("park".to_string(), "spake".to_string()));
+    println!("{:?}", min_distance::min_distance("horse".to_string(), "ros".to_string()));
 }
--- rust/src/min_distance.rs
+++ rust/src/min_distance.rs
@@ -1,30 +1,26 @@
-use std::collections::VecDeque;
+use std::cmp::min;
 
 pub fn min_distance(word1: String, word2: String) -> i32 {
     let word1: &[u8] = word1.as_bytes();
     let word2: &[u8] = word2.as_bytes();
-    let mut queue: VecDeque<(i32, usize, usize)> = VecDeque::new(); // word1[..second] are first away from word2[..third]
-    let mut current: (i32, usize, usize) = (0, 0, 0);
-    while current.1 != word1.len() || current.2 != word2.len() {
-        if current.1 == word1.len() {
-            // extend current.2
-            queue.push_back((current.0 + 1, current.1, current.2 + 1));
-        } else if current.2 == word2.len() {
-            // extend current.1
-            queue.push_back((current.0 + 1, current.1 + 1, current.2));
-        } // at this point both word1.get(current.1) and word2.get(current.2) will be fine
-        else if word1.get(current.1) == word2.get(current.2) {
-            // does match
-            queue.push_front((current.0, current.1 + 1, current.2 + 1));
-        } else { // doesn't match
-            // change current char
-            queue.push_back((current.0 + 1, current.1 + 1, current.2 + 1));
-            // insert new char in one of them (equivalent to removing one in another)
-            queue.push_back((current.0 + 1, current.1 + 1, current.2));
-            queue.push_back((current.0 + 1, current.1, current.2 + 1));
-        }
-        current = queue.pop_front().unwrap();
-        println!("{}, {}, {}", current.0, current.1, current.2);
+    let mut dp: Vec<Vec<i32>> = Vec::with_capacity(word1.len());
+    dp.push(Vec::with_capacity(word2.len()));
+    for j in 0..word2.len() {
+        dp[0].push(j as i32);
     }
-    return current.0;
+    for i in 1..word1.len() {
+        dp.push(Vec::with_capacity(word2.len()));
+        let s = dp[i-1][0] + 1;
+        dp[i].push(s);
+        for j in 1..word2.len() {
+            let l1 = dp[i][j-1] + 1;
+            let l2 = dp[i-1][j] + 1;
+            let mut l3 = dp[i-1][j-1];
+            if word1[i] != word2[j] {
+                l3 += 1;
+            }
+            dp[i].push(min(l1, min(l2, l3)));
+        }
+    }
+    return dp[word1.len() - 1][word2.len() - 1];
 }

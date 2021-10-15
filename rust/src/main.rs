5b9f05c7d668a93b899c2ec2f394b69ed16ed74c
chomosuke <a13323600@gmail.com>
2021-10-16 05:50:46 +1100

    493B

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -54,5 +54,45 @@ impl Scanner {
 }
 
 fn main() {
-    
+    let mut sc = Scanner::new();
+    let n = sc.next::<i32>();
+    let mut first_score = Vec::<i32>::new();
+    let mut second_score = Vec::<i32>::new();
+    let mut sum: i64 = 0;
+    let mut first_last = true;
+    for _ in 0..n {
+        let a = sc.next::<i32>();
+        if a > 0 {
+            first_score.push(a);
+            first_last = true;
+        } else {
+            second_score.push(a);
+            first_last = false;
+        }
+        sum += a as i64;
+    }
+    if sum > 0 {
+        println!("first");
+    } else if sum < 0 {
+        println!("second");
+    } else {
+        for (f, s) in first_score.iter().zip(second_score.iter()) {
+            if f + s > 0 {
+                println!("first");
+                return;
+            } else if f + s < 0 {
+                println!("second");
+                return;
+            }
+        }
+        if first_score.len() > second_score.len() {
+            println!("first");
+        } else if first_score.len() < second_score.len() {
+            println!("second");
+        } else if first_last {
+            println!("first");
+        } else {
+            println!("second");
+        }
+    }
 }

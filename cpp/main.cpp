bd9b5b1e6da093e0829dbbb83deae17be51815fe
chomosuke <a13323600@gmail.com>
2022-08-20 14:19:22 +1000

    Subliminal Messaging umcpc

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -10,22 +11,33 @@ typedef long long ll;
 int main() {
     ll n;
     cin >> n;
-    ll total = -1;
-    ll pp, p;
-    cin >> pp >> p;
-    bool longing = pp < p;
-    total += abs(pp - p);
-    pp = p;
-    for (ll i = 0; i < n - 2; i++) {
-        cin >> p;
-
-        total += abs(pp - p);
-        if (pp < p != longing) {
-            total -= 1;
+    vector<vector<char>> grid;
+    for (ll i = 0; i < n; i++) {
+        vector<char> row;
+        grid.push_back(row);
+        for (ll j = 0; j < n; j++) {
+            char c;
+            cin >> c;
+            grid[i].push_back(c);
         }
-        longing = pp < p;
-
-        pp = p;
     }
-    cout << total << endl;
+    for (ll i = 0; i < n; i++) {
+        for (ll j = 0; j < n; j++) {
+            bool print = true;
+            for (ll k = 0; k < n; k++) {
+                if (k != i && grid[i][j] == grid[k][j]) {
+                    print = false;
+                    break;
+                }
+                if (k != j && grid[i][j] == grid[i][k]) {
+                    print = false;
+                    break;
+                }
+            }
+            if (print) {
+                cout << grid[i][j];
+            }
+        }
+    }
+    cout << endl;
 }

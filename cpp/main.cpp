dfc9cdc9e4433354ea0798e5d33dc2d67b81d234
chomosuke <a13323600@gmail.com>
2022-08-20 13:49:49 +1000

    crypto prophet umcpc

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -8,48 +8,24 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    ll c, u;
-    cin >> c >> u;
-    vector<ll> sizes;
-    for (int i = 0; i < c; i++) {
-        ll ci;
-        cin >> ci;
-        sizes.push_back(ci);
-    }
+    ll n;
+    cin >> n;
+    ll total = -1;
+    ll pp, p;
+    cin >> pp >> p;
+    bool longing = pp < p;
+    total += abs(pp - p);
+    pp = p;
+    for (ll i = 0; i < n - 2; i++) {
+        cin >> p;
 
-    ll sum = 0, biggest = 0;
-    for (auto& n : sizes) {
-        sum += n;
-    }
-    for (auto& n : sizes) {
-        biggest = max(biggest, n);
-    }
-
-    if (sum % 2 != 0) {
-        cout << "no" << endl;
-    } else {
-        if (biggest <= sum / 2) {
-            cout << "yes" << endl;
-        } else {
-            cout << "no" << endl;
+        total += abs(pp - p);
+        if (pp < p != longing) {
+            total -= 1;
         }
-    }
+        longing = pp < p;
 
-    for (int i = 0; i < u; i++) {
-        ll j, x;
-        cin >> j >> x;
-        sizes[j - 1] += x;
-        biggest = max(biggest, sizes[j - 1]);
-        sum += x;
-
-        if (sum % 2 != 0) {
-            cout << "no" << endl;
-        } else {
-            if (biggest <= sum / 2) {
-                cout << "yes" << endl;
-            } else {
-                cout << "no" << endl;
-            }
-        }
+        pp = p;
     }
+    cout << total << endl;
 }

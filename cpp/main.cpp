f88e5d46184a575073151b042a8adb74dedd4b5d
chomosuke <a13323600@gmail.com>
2022-09-01 18:14:27 +1000

    Competitive programmer's handbook Chapter 3 finished

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -11,5 +11,45 @@ using namespace std;
 typedef intmax_t Int;
 typedef long double Float;
 
+void print_vector(const vector<Int> &v) {
+    if (v.size() == 0) {
+        return;
+    }
+    cout << v[0];
+    for (Int i = 1; i < v.size(); i++) {
+        cout << " " << v[i];
+    }
+    cout << endl;
+}
+
+Int find(const vector<Int> &v, Int target) {
+    Int i = 0;
+    for (Int j = v.size() / 2; j >= 1; j /= 2) {
+        while (i+j < v.size() && v[i+j] <= target) {
+            i += j;
+        }
+    }
+    if (v[i] == target) {
+        return i;
+    } else {
+        return -1;
+    }
+}
+
 int main() {
+    vector<Int> v = {4, 2, 5, 3, 5, 8, 3};
+    sort(v.begin(), v.end());
+    print_vector(v);
+    sort(v.rbegin(), v.rend());
+    print_vector(v);
+
+    // default sort comparitor is [](Int a, Int b) { return a < b; }
+    sort(v.begin(), v.end(), [](Int a, Int b) { return a < b; });
+    print_vector(v);
+
+    while (true) {
+        Int target;
+        cin >> target;
+        cout << find(v, target) << endl;
+    }
 }

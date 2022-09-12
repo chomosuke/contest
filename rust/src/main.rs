3a2eb69a5acf8c097fd6714e34038e4a26eca931
chomosuke <a13323600@gmail.com>
2022-09-12 20:36:58 +1000

    half ass solved counting tilings

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,8 +5,8 @@ use std::collections::*;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::io::stdin;
-use std::str::FromStr;
 use std::ops::Bound::*;
+use std::str::FromStr;
 
 struct Scanner {
     tokens: VecDeque<String>,
@@ -101,25 +101,42 @@ type U = usize;
 
 fn main() {
     let mut sc = Scanner::new();
-    let str1 = sc.next_line();
-    let str2 = sc.next_line();
-    println!("{}", distance(str1.as_bytes(), str2.as_bytes(), 0, 0, &mut HashMap::new()))
+    let m = sc.next::<I>();
+    let n = sc.next::<U>();
+    println!("{}", count(&vec![0; n], m))
 }
 
-fn distance(str1: &[u8], str2: &[u8], i1: U, i2: U, memoize: &mut HashMap<(U, U), U>) -> U {
-    if let Some(&d) = memoize.get(&(i1, i2)) {
-        return d;
+// symbols are n u ( )
+fn count(row: &[u8], m: I) -> I {
+    if m == 0 {
+        for &x in row {
+            if x == b'u' {
+                return 0;
+            }
+        }
+        return 1;
     }
-    if i1 == str1.len() {
-        return str2.len() - i2;
+    enumerate(row, &mut vec![0; row.len()], 0, m)
+}
+
+fn enumerate(row: &[u8], next_row: &mut Vec<u8>, i: U, m: I) -> I {
+    if i == next_row.len() {
+        return count(next_row, m - 1);
     }
-    if i2 == str2.len() {
-        return str1.len() - i1;
+    if row[i] == b'u' {
+        next_row[i] = b'n';
+        return enumerate(row, next_row, i + 1, m);
     }
-    let cost = if str1[i1] == str2[i2] { 0 } else { 1 };
-    let d = (distance(str1, str2, i1 + 1, i2, memoize) + 1)
-                   .min(distance(str1, str2, i1, i2 + 1, memoize) + 1)
-                   .min(distance(str1, str2, i1 + 1, i2 + 1, memoize) + cost);
-    memoize.insert((i1, i2), d);
-    d
+    if i > 0 && next_row[i - 1] == b'(' {
+        next_row[i] = b')';
+        return enumerate(row, next_row, i + 1, m);
+    }
+    let mut sum = 0;
+    if i < next_row.len() - 1 {
+        next_row[i] = b'(';
+        sum += enumerate(row, next_row, i + 1, m);
+    }
+    next_row[i] = b'u';
+    sum += enumerate(row, next_row, i + 1, m);
+    sum
 }

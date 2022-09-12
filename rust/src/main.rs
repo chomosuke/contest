d6a227b7e4a885d4738d3b665398609e48a0c84c
chomosuke <a13323600@gmail.com>
2022-09-13 00:39:14 +1000

    466C

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -94,59 +94,15 @@ impl<E: Eq + Hash> MultiSet<E> {
     }
 }
 
-#[allow(dead_code)]
-type I = i128;
-#[allow(dead_code)]
-type U = usize;
-
-fn main() {
-    let mut sc = Scanner::new();
-    let t = sc.next::<U>();
-    for _ in 0..t {
-        let n = sc.next::<U>();
-        let q = sc.next::<U>();
-        let mut a = Vec::with_capacity(n);
-        let mut s = HashMap::<I, Vec<U>>::with_capacity(q);
-        for _ in 0..n {
-            a.push(sc.next::<I>());
-        }
-        a.sort();
-        let mut sum_so_far = Vec::with_capacity(n);
-        sum_so_far.push(0);
-        for ai in &a {
-            sum_so_far.push(sum_so_far.last().unwrap() + ai);
-        }
-        for i in 0..q {
-            let si = sc.next::<I>();
-            let mut ls = s.remove(&si).unwrap_or_default();
-            ls.push(i);
-            s.insert(si, ls);
-        }
-        let mut ans = vec![false; q];
-        tests(&a, &sum_so_far, &s, &mut ans);
-        for p in ans {
-            if p {
-                println!("Yes");
-            } else {
-                println!("No");
-            }
-        }
-    }
+trait BinarySearchable<T> {
+    fn binary_search_leq(&self, x: &T) -> usize;
+    fn binary_search_geq(&self, x: &T) -> usize;
 }
 
-fn tests(a: &[I], sum_so_far: &[I], s: &HashMap<I, Vec<U>>, ans: &mut [bool]) {
-    if let Some(ls) = s.get(&(sum_so_far.last().unwrap() - sum_so_far[0])) {
-        for &l in ls {
-            ans[l] = true;
-        }
-    }
-    if a.len() <= 1 {
-        return;
-    }
-    let mid = (a[0] + a.last().unwrap()) / 2;
-    let split = a
-        .binary_search_by(|ai| {
-            let r = ai.cmp(&mid);
+impl<T: Ord> BinarySearchable<T> for [T] {
+    fn binary_search_leq(&self, x: &T) -> usize {
+        self.binary_search_by(|p| {
+            let r = p.cmp(x);
             if r == Ordering::Greater {
                 Ordering::Greater
             } else {
@@ -154,10 +110,69 @@ fn tests(a: &[I], sum_so_far: &[I], s: &HashMap<I, Vec<U>>, ans: &mut [bool]) {
             }
         })
         .err()
-        .unwrap();
-    if split == a.len() {
+        .unwrap()
+    }
+
+    fn binary_search_geq(&self, x: &T) -> usize {
+        self.binary_search_by(|p| {
+            let r = p.cmp(x);
+            if r == Ordering::Less {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        })
+        .err()
+        .unwrap()
+    }
+}
+
+#[allow(dead_code)]
+type I = i128;
+#[allow(dead_code)]
+type U = usize;
+
+fn main() {
+    let mut sc = Scanner::new();
+    let n = sc.next::<U>();
+    let mut a = Vec::with_capacity(n);
+    for _ in 0..n {
+        a.push(sc.next::<I>());
+    }
+    let sum: I = a.iter().sum();
+    let target = sum / 3;
+    if sum != target * 3 {
+        println!("0");
         return;
     }
-    tests(&a[..split], &sum_so_far[..(split + 1)], s, ans);
-    tests(&a[split..], &sum_so_far[split..], s, ans);
+    let mut sum = 0;
+    let mut target_indexes = Vec::new();
+    for (i, ai) in a.iter().enumerate().take(a.len() - 1) {
+        sum += ai;
+        if sum == target {
+            target_indexes.push(i);
+        }
+    }
+    sum = 0;
+    let mut target_indexes_rev = Vec::new();
+    for (i, ai) in a.iter().enumerate().rev().take(a.len() - 1) {
+        sum += ai;
+        if sum == target {
+            target_indexes_rev.push(i - 1);
+        }
+    }
+    let target_indexes_rev: Vec<U> = target_indexes_rev.into_iter().rev().collect();
+    let (mut i, mut j) = (0, 0);
+    let mut count = 0;
+    'outer: while i < target_indexes.len() && j < target_indexes_rev.len() {
+        while target_indexes[i] >= target_indexes_rev[j] {
+            j += 1;
+            if j >= target_indexes_rev.len() {
+                break 'outer;
+            }
+        }
+        count += target_indexes_rev.len() - j;
+        i += 1;
+    }
+    println!("{}", count);
 }

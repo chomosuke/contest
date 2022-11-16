75c27da2b35e6ee4e23ffa4e07b4ec436a0c8a89
chomosuke <a13323600@gmail.com>
2022-11-17 03:55:24 +1100

    renamed collection.rs no multi_set.rs

--- rust/lib/src/lib.rs
+++ rust/lib/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::new_without_default, clippy::needless_range_loop)]
 
 pub mod binary_searchable;
-pub mod collections;
+pub mod multi_set;
 pub mod graph;
 pub mod indexed_vec;
 pub mod math;

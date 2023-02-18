5482257d398ccf094dc0c3aaa4750cbf905abf10
chomosuke <a13323600@gmail.com>
2023-02-19 02:50:38 +1100

    report error if cpp doesn't compile
    because tony learns from his mistake

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 clang++ main.cpp -Wall -o main.exe
+exitcode=$?
+RED='\033[0;31m'
+NC='\033[0m'
+if [ $exitcode -ne 0 ]; then
+	echo -e "${RED}compilation failed${NC}"
+	exit 1
+fi
 echo compiled
 ./main.exe

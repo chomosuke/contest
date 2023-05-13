710ece6d17664fa70367f296cbc30d3e36e4a761
chomosuke <a13323600@gmail.com>
2023-05-13 20:55:58 +1000

    scanner from file, 35C

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,46 +2,69 @@
 use std::{
     cmp::{max, min, Ordering},
     collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
+    fs,
+    io::BufReader,
 };
 
+fn dist((x1, y1): (u64, u64), (x2, y2): (u64, u64)) -> u64 {
+    ((x1 as i128 - x2 as i128).abs() + (y1 as i128 - y2 as i128).abs()) as u64
+}
+
 fn main() {
-    let mut sc = Scanner::new();
+    let mut sc = Scanner::new(fs::File::open("input.txt").unwrap());
     let n = sc.next::<u64>();
-    if n < 6 {
-        println!("-1");
-    } else {
-        println!("1 2");
-        println!("1 3");
-        println!("1 4");
-        for i in 5..=n {
-            println!("2 {}", i);
+    let m = sc.next::<u64>();
+    let k = sc.next::<usize>();
+    let mut bs = Vec::with_capacity(k);
+    for _ in 0..k {
+        let x = sc.next::<u64>();
+        let y = sc.next::<u64>();
+        bs.push((x, y));
+    }
+    let mut max_dist = 0;
+    let mut max_dist_xy = bs[0];
+    for x in 1..=n {
+        for y in 1..=m {
+            let mut min_dist = m + n;
+            for b in &bs {
+                let dist = dist(*b, (x, y));
+                if dist < min_dist {
+                    min_dist = dist;
+                }
+            }
+            if min_dist > max_dist {
+                max_dist_xy = (x, y);
+                max_dist = min_dist;
+            }
         }
     }
-    for i in 2..=n {
-        println!("{} {}", i-1, i);
-    }
+    fs::write("output.txt", format!("{} {}", max_dist_xy.0, max_dist_xy.1)).unwrap();
 }
 
 mod scanner {
     use std::collections::{HashSet, VecDeque};
-    use std::{any::type_name, io::stdin, str::FromStr};
+    use std::io::{BufReader, Read};
+    use std::{any::type_name, io::BufRead, str::FromStr};
 
-    pub struct Scanner {
+    pub struct Scanner<R: Read> {
         tokens: VecDeque<String>,
         delimiters: Option<HashSet<char>>,
+        source: BufReader<R>,
     }
-    impl Scanner {
-        pub fn new() -> Self {
+    impl<R: Read> Scanner<R> {
+        pub fn new(source: R) -> Self {
             Self {
                 tokens: VecDeque::new(),
                 delimiters: None,
+                source: BufReader::new(source),
             }
         }
 
-        pub fn with_delimiters(delimiters: &[char]) -> Self {
+        pub fn with_delimiters(source: R, delimiters: &[char]) -> Self {
             Self {
                 tokens: VecDeque::new(),
                 delimiters: Some(delimiters.iter().copied().collect()),
+                source: BufReader::new(source),
             }
         }
 
@@ -61,14 +84,14 @@ mod scanner {
         pub fn next_line(&mut self) -> String {
             assert!(self.tokens.is_empty(), "You have unprocessed token");
             let mut line = String::new();
-            stdin().read_line(&mut line).expect("Failed to read.");
+            self.source.read_line(&mut line).expect("Failed to read.");
             line.pop();
             line
         }
 
         fn receive_input(&mut self) {
             let mut line = String::new();
-            stdin().read_line(&mut line).expect("Failed to read.");
+            self.source.read_line(&mut line).expect("Failed to read.");
             if let Some(delimiters) = &self.delimiters {
                 for token in line.split(|c| delimiters.contains(&c)) {
                     self.tokens.push_back(token.to_string());

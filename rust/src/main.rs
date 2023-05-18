25bb17cecc1edf9ba47ff2a85dbaa24e70604697
chomosuke <a13323600@gmail.com>
2023-05-18 18:53:38 +1000

    295B

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,46 +3,65 @@ use std::{
     cmp::{max, min, Ordering},
     collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
     fs,
-    io::{BufReader, stdin},
+    io::{stdin, BufReader},
 };
 
 fn main() {
     let mut sc = Scanner::new(stdin());
-    let test_cases = sc.next::<usize>();
-    'outer: for _ in 0..test_cases {
-        let _n = sc.next::<usize>();
-        let a = sc.next_line().into_bytes();
-        let b = sc.next_line().into_bytes();
-        let mut counts = vec![vec![false; 20]; 20];
-        for (a, b) in a.into_iter().zip(b.into_iter()) {
-            let a = a - b'a';
-            let b = b - b'a';
-            if a > b {
-                println!("-1");
-                continue 'outer;
-            }
-            if a < b {
-                counts[a as usize][b as usize] = true;
-            }
-        }
-        let mut moves = 0;
-        for a in 0..20 {
-            let min_b = counts[a].iter().position(|&t| t);
-            if let Some(min_b) = min_b {
-                for b in (min_b + 1)..20 {
-                    counts[min_b][b] = counts[min_b][b] || counts[a][b];
-                }
-                moves += 1;
-            }
-        }
-        println!("{moves}");
+    let n = sc.next::<usize>();
+    let mut adj_mat = Vec::with_capacity(n);
+    for _ in 0..n {
+        let adj_row = sc.next_n::<u64>(n).collect::<Vec<_>>();
+        adj_mat.push(adj_row);
     }
+    let delete = sc.next_n::<usize>(n).map(|v| v - 1).collect::<Vec<_>>();
+    let mut sums = Vec::with_capacity(n);
+    for (i, &ni) in delete.iter().rev().enumerate() {
+        for n1 in 0..n {
+            for n2 in 0..n {
+                adj_mat[n1][n2] = min(adj_mat[n1][ni] + adj_mat[ni][n2], adj_mat[n1][n2]);
+            }
+        }
+        let contained = &delete[(n - i - 1)..];
+        let mut sum = 0;
+        for &n1 in contained {
+            for &n2 in contained {
+                sum += adj_mat[n1][n2];
+            }
+        }
+        sums.push(sum);
+    }
+    print!("{}", sums.pop().unwrap());
+    for s in sums.into_iter().rev() {
+        print!(" {s}");
+    }
+    println!();
 }
 
 mod scanner {
     use std::collections::{HashSet, VecDeque};
-    use std::io::{BufReader, Read, Lines};
+    use std::io::{BufReader, Lines, Read};
+    use std::marker::PhantomData;
     use std::{any::type_name, io::BufRead, str::FromStr};
+
+    pub struct ScannerIter<'a, R: Read, T> {
+        remaining: usize,
+        sc: &'a mut Scanner<R>,
+        item: PhantomData<T>,
+    }
+
+    impl<R: Read, T: FromStr> Iterator for ScannerIter<'_, R, T> {
+        type Item = T;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.remaining == 0 {
+                None
+            } else {
+                self.remaining -= 1;
+                Some(self.sc.next::<T>())
+            }
+        }
+    }
 
     pub struct Scanner<R: Read> {
         tokens: VecDeque<String>,
@@ -79,14 +98,28 @@ mod scanner {
                 .unwrap_or_else(|_| panic!("input {} isn't a {}", token, type_name::<T>()))
         }
 
+        pub fn next_n<T: FromStr>(&mut self, n: usize) -> ScannerIter<'_, R, T> {
+            ScannerIter {
+                remaining: n,
+                sc: self,
+                item: PhantomData,
+            }
+        }
+
         pub fn next_line(&mut self) -> String {
             assert!(self.tokens.is_empty(), "You have unprocessed token");
-            let line = self.lines.next().and_then(|e| e.ok()).expect("Failed to read.");
-            line
+            self.lines
+                .next()
+                .and_then(|e| e.ok())
+                .expect("Failed to read.")
         }
 
         fn receive_input(&mut self) {
-            let line = self.lines.next().and_then(|e| e.ok()).expect("Failed to read.");
+            let line = self
+                .lines
+                .next()
+                .and_then(|e| e.ok())
+                .expect("Failed to read.");
             if let Some(delimiters) = &self.delimiters {
                 for token in line.split(|c| delimiters.contains(&c)) {
                     self.tokens.push_back(token.to_string());

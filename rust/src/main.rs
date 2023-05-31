5db5bf4f3de82f2c42fa9526c4b2a4cb2c5d3d22
chomosuke <a13323600@gmail.com>
2023-06-01 01:33:22 +1000

    buffer printer & 1513C

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,27 +3,39 @@ use std::{
     cmp::{max, min, Ordering},
     collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
     fs,
-    io::{stdin, BufReader},
+    io::{stdin, stdout, BufReader},
 };
 
 fn main() {
+    let modu = 1_000_000_007u64;
+    let mut brr = vec![1; 10];
+    for i in 10..=200_100 {
+        brr.push((brr[i - 10] + brr[i - 9]) % modu);
+    }
+
     let mut sc = Scanner::new(stdin());
-    let n = sc.next::<usize>();
-    let arr = sc.next_n(n).collect::<Vec<usize>>();
-    let &max = arr.iter().max().unwrap();
-    let sat_rounds = arr.iter().map(|a| max - a).sum::<usize>();
-    if sat_rounds >= max {
-        println!("{max}");
-    } else {
-        let rounds_full = max - sat_rounds;
-        println!("{}", sat_rounds + ((rounds_full * n) as f64 / (n - 1) as f64).ceil() as usize);
+    let mut pt = Printer::new(stdout());
+    let test_cases = sc.next::<usize>();
+    for _ in 0..test_cases {
+        let n = sc
+            .next::<String>()
+            .into_bytes()
+            .into_iter()
+            .map(|d| (d - b'0') as usize);
+        let m = sc.next::<usize>();
+        let mut len = 0;
+        for d in n {
+            len += brr[m + d];
+            len %= modu;
+        }
+        pt.println(&len);
     }
 }
 
-mod scanner {
+mod io {
     use std::collections::{HashSet, VecDeque};
     use std::fmt::Display;
-    use std::io::{BufReader, Lines, Read};
+    use std::io::{BufReader, BufWriter, Lines, Read, Write};
     use std::marker::PhantomData;
     use std::{any::type_name, io::BufRead, str::FromStr};
 
@@ -115,15 +127,40 @@ mod scanner {
         }
     }
 
-    pub fn print_iter(mut iter: impl Iterator<Item = impl Display>) {
-        if let Some(e) = iter.next() {
-            print!("{e}");
-            for e in iter {
-                print!(" {e}");
+    pub struct Printer<W: Write> {
+        writer: BufWriter<W>,
+    }
+    impl<W: Write> Printer<W> {
+        pub fn new(destination: W) -> Self {
+            Self {
+                writer: BufWriter::new(destination),
             }
         }
-        println!();
+
+        pub fn print(&mut self, s: &impl Display) {
+            self.writer.write_all(s.to_string().as_bytes()).unwrap();
+        }
+
+        pub fn print_bytes(&mut self, b: &[u8]) {
+            self.writer.write_all(b).unwrap();
+        }
+
+        pub fn println(&mut self, s: &impl Display) {
+            self.print(s);
+            self.print_bytes(&[b'\n']);
+        }
+
+        pub fn print_iter(&mut self, mut iter: impl Iterator<Item = impl Display>) {
+            if let Some(e) = iter.next() {
+                self.print(&e);
+                for e in iter {
+                    self.print_bytes(&[b' ']);
+                    self.print(&e);
+                }
+            }
+            self.print_bytes(&[b'\n']);
+        }
     }
 }
 #[allow(unused_imports)]
-use scanner::*;
+use io::*;

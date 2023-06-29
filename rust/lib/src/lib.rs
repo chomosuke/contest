4ed01b4e762aaa5b187948cb712e554097c8fc64
chomosuke <a13323600@gmail.com>
2023-06-30 02:46:41 +1200

    moved lib to another repo

--- rust/lib/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![allow(clippy::new_without_default, clippy::needless_range_loop)]
-
-pub mod binary_searchable;
-pub mod multi_set;
-pub mod graph;
-pub mod indexed_vec;
-pub mod math;
-pub mod knapsack;

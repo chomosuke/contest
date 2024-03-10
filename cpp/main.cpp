9cf765e973692160d7309a158dbca2a432c5b97d
chomosuke <a13323600@gmail.com>
2024-03-10 20:51:09 +1100

    Lambda calculus from Quang

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,160 +1,115 @@
-#include <algorithm>
-#include <bitset>
-#include <cmath>
-#include <cstdint>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-
-namespace hash_tuple {
-template <typename TT> struct hash {
-    size_t operator()(TT const &tt) const { return std::hash<TT>()(tt); }
-};
-namespace {
-
-template <class T> inline void hash_combine(std::size_t &seed, T const &v) {
-    seed ^= hash_tuple::hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
-
-template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
-struct HashValueImpl {
-    static void apply(size_t &seed, Tuple const &tuple) {
-        HashValueImpl<Tuple, Index - 1>::apply(seed, tuple);
-        hash_combine(seed, std::get<Index>(tuple));
-    }
-};
-
-template <class Tuple> struct HashValueImpl<Tuple, 0> {
-    static void apply(size_t &seed, Tuple const &tuple) {
-        hash_combine(seed, std::get<0>(tuple));
-    }
-};
-} // namespace
-
-template <typename... TT> struct hash<std::tuple<TT...>> {
-    size_t operator()(std::tuple<TT...> const &tt) const {
-        size_t seed = 0;
-        HashValueImpl<std::tuple<TT...>>::apply(seed, tt);
-        return seed;
-    }
-};
-} // namespace hash_tuple
-
-// unordered_map<tuple<Int, Float>, Int, hash_tuple::hash<tuple<Int, Float>>>
-//     memoize;
-
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef int64_t Int;
-const Int Int_max = INT64_MAX;
-const Int Int_min = INT64_MIN;
-typedef long double Float;
-
-struct CycleLenCount {
-    Int len;
-    Int count;
+struct Variable {
+    int name;
+    bool operator==(const Variable &oth) const { return name == oth.name; }
+    bool operator!=(const Variable &oth) const { return !(operator==(oth)); }
 };
 
-unordered_map<tuple<Int, Int>, Int, hash_tuple::hash<tuple<Int, Int>>> memoize;
+struct Abstraction;
+struct Application;
 
-// how many swap do we need if only cycle_len_counts[i, end) can be used
-Int count_swaps(vector<CycleLenCount> const &cycle_len_counts, Int i,
-                Int const target) {
-    if (memoize.find(tuple(i, target)) != memoize.end()) {
-        return memoize[tuple(i, target)];
-    }
-    Int swaps = Int_max / 2;
-    if (i >= cycle_len_counts.size()) {
-        return swaps;
-    }
-    for (Int include = 0; include <= cycle_len_counts[i].count; include++) {
-        if (target - include * cycle_len_counts[i].len > 0) {
-            swaps =
-                min(swaps,
-                    include + count_swaps(
-                                  cycle_len_counts, i + 1,
-                                  target - include * cycle_len_counts[i].len));
-        } else if (target - include * cycle_len_counts[i].len == 0) {
-            swaps = min(swaps, include - 1);
+typedef variant<Variable, Abstraction, Application> Lambda;
+
+struct Abstraction {
+    Variable input;
+    Lambda *term;
+};
+
+struct Application {
+    Lambda *input;
+    Lambda *output;
+};
+
+class LambdaCalculus {
+  public:
+    Lambda *Parse(const string &str) {}
+
+    string ToString(Lambda *term) {
+        if (holds_alternative<Variable>(*term)) {
+            return to_string(get<Variable>(*term).name);
+        } else if (holds_alternative<Application>(*term)) {
+            const Application &app = get<Application>(*term);
+            string left = ToString(app.input);
+            string right = ToString(app.output);
+            return "(" + left + ") (" + right + ")";
         } else {
-            break;
+            Abstraction &abst = get<Abstraction>(*term);
+            return "lambda" + to_string(abst.input.name) + ": " +
+                   ToString(abst.term);
         }
     }
-    memoize[tuple(i, target)] = swaps;
-    return swaps;
-}
 
-int main() {
-    Int case_count;
-    cin >> case_count;
-    for (Int case_number = 1; case_number <= case_count; case_number++) {
-        memoize.clear();
-
-        Int n;
-        cin >> n;
-        Int *ps = new Int[n];
-        for (Int i = 0; i < n; i++) {
-            Int p;
-            cin >> p;
-            ps[i] = p - 1;
+    unordered_set<Variable> FreeVariables(const Lambda &term) {
+        if (holds_alternative<Variable>(term)) {
+            return {get<Variable>(term)};
+        } else if (holds_alternative<Application>(term)) {
+            const Application &app = get<Application>(term);
+            auto left = FreeVariables(*app.input);
+            auto right = FreeVariables(*app.output);
+            left.merge(right);
+            return left;
+        } else {
+            Abstraction &abst = get<Abstraction>(term);
+            auto freeVars = FreeVariables(*abst.term);
+            freeVars.erase(abst.input);
+            return freeVars;
         }
+    }
 
-        bool *visited = new bool[n];
-        for (int i = 0; i < n; i++) {
-            visited[i] = false;
-        }
-        unordered_map<Int, Int> cycle_counts;
-        Int i = 0;
-        while (i < n) {
-            visited[i] = true;
-            Int j = ps[i];
-            Int length = 1;
-            while (j != i) {
-                length++;
-                visited[j] = true;
-                j = ps[j];
+    void Substitute(Lambda *term, const Variable &variable,
+                    const Lambda &replacement) {
+        if (holds_alternative<Variable>(*term)) {
+            if (get<Variable>(*term) == variable) {
+                *term = replacement;
             }
-            cycle_counts[length]++;
-            while (i < n && visited[i]) {
-                i++;
-            }
-        }
-
-        vector<CycleLenCount> cycle_len_counts;
-        for (auto it = cycle_counts.begin(); it != cycle_counts.end(); it++) {
-            cycle_len_counts.push_back({.len = it->first, .count = it->second});
-        }
-
-        sort(cycle_len_counts.begin(), cycle_len_counts.end(),
-             [](CycleLenCount a, CycleLenCount b) { return a.len > b.len; });
-
-        cout << "Case #" << case_number << ":";
-
-        for (Int target = 1; target <= n; target++) {
-            // two senario
-            // 1. n biggest cycle add together and cut one to form the target =
-            // n + 1
-            Int sum = 0;
-            Int swaps = 0;
-            for (Int i = 0; true; i++) {
-                if (sum + cycle_len_counts[i].len * cycle_len_counts[i].count >=
-                    target) {
-                    swaps +=
-                        ceil((double)(target - sum) / cycle_len_counts[i].len);
-                    break;
-                } else {
-                    sum += cycle_len_counts[i].len * cycle_len_counts[i].count;
-                    swaps += cycle_len_counts[i].count;
+        } else if (holds_alternative<Application>(*term)) {
+            Application &app = get<Application>(*term);
+            Substitute(app.input, variable, replacement);
+            Substitute(app.output, variable, replacement);
+        } else {
+            Abstraction &abst = get<Abstraction>(*term);
+            if (abst.input != variable) {
+                const auto &freeVars = FreeVariables(replacement);
+                if (freeVars.find(variable) == freeVars.end()) {
+                    Substitute(abst.term, variable, replacement);
                 }
             }
-            // 2. n cycle add together to form the target = n
-            swaps = min(swaps, count_swaps(cycle_len_counts, 0, target));
-            cout << " " << swaps;
         }
-        cout << endl;
     }
+
+    void Reduce(Lambda *term) {
+        if (holds_alternative<Variable>(*term))
+            return;
+
+        if (holds_alternative<Application>(*term)) {
+            Application &app = get<Application>(*term);
+            if (holds_alternative<Abstraction>(*app.input)) {
+                Abstraction &abst = get<Abstraction>(*app.input);
+                Lambda newTerm = {*abst.term};
+                Substitute(&newTerm, abst.input, *app.output);
+                if (*term != newTerm) {
+                    /* This is not full cycle detection */
+                    *term = newTerm;
+                    Reduce(term);
+                }
+            }
+        } else {
+            Abstraction &abst = get<Abstraction>(*term);
+            Reduce(abst.term);
+        }
+    }
+
+  private:
+    vector<unique_ptr<Lambda>> mTerms;
+};
+
+int main() {
+    string input;
+    cin >> input;
+    LambdaCalculus lc;
+    Lambda *lambda = lc.Parse(input);
+    lc.Reduce(lambda);
+    cout << lc.ToString(lambda);
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,168 +1,28 @@
+#[allow(unused_imports)]
 use std::{
     cmp::min,
     collections::{HashMap, VecDeque},
     io::{self, BufRead},
 };
 
-struct Customer {
-    id: i64,
-    num_items: i32,
-    processed_items: i32,
-}
+struct Scheduler {}
 
-struct Line {
-    customers: VecDeque<Option<Customer>>,
-    poped_front: usize,
-}
-
-// #[derive(Hash)]
-struct LinePos {
-    line_number: i64,
-    line_pos: usize,
-}
-
-pub struct SupermarketCheckout {
-    lines: HashMap<i64, Line>,
-    customers_line_pos: HashMap<i64, LinePos>,
-}
-
-fn on_customer_exit(customer_id: i64) {
-    // Don't change this implementation.
-    println!("{}", customer_id);
-}
-
-impl SupermarketCheckout {
-    pub fn new() -> Self {
-        Self {
-            lines: HashMap::new(),
-            customers_line_pos: HashMap::new(),
-        }
+impl Scheduler {
+    fn new() -> Self {
+        todo!()
     }
 
-    pub fn on_customer_enter(&mut self, customer_id: i64, line_number: i64, num_items: i32) {
-        if !self.lines.contains_key(&line_number) {
-            self.lines.insert(
-                line_number,
-                Line {
-                    customers: VecDeque::new(),
-                    poped_front: 0,
-                },
-            );
-        }
-        self.customers_line_pos.insert(
-            customer_id,
-            LinePos {
-                line_number,
-                line_pos: self.lines[&line_number].customers.len(),
-            },
-        );
-        self.lines
-            .get_mut(&line_number)
-            .unwrap()
-            .customers
-            .push_back(Some(Customer {
-                id: customer_id,
-                num_items,
-                processed_items: 0,
-            }));
+    fn new_job(&mut self, id: usize, steps: impl Iterator) {
+        todo!()
     }
 
-    pub fn on_basket_change(&mut self, customer_id: i64, new_num_items: i32) {
-        let LinePos {
-            line_number,
-            line_pos,
-        } = self.customers_line_pos.get_mut(&customer_id).unwrap();
-        let line = self.lines.get_mut(line_number).unwrap();
-        let pos = *line_pos - line.poped_front;
-        let customer_opt = &mut line.customers[pos];
-        let customer = customer_opt.as_mut().unwrap();
-        if new_num_items > customer.num_items + customer.processed_items {
-            let mut customer = customer_opt.take().unwrap();
-            customer.num_items = new_num_items - customer.processed_items;
-            *line_pos = line.poped_front + line.customers.len();
-            line.customers.push_back(Some(customer));
-        } else {
-            customer.num_items = new_num_items - customer.processed_items;
-            if new_num_items == 0 {
-                customer_opt.take();
-                self.customers_line_pos.remove(&customer_id);
-                on_customer_exit(customer_id);
-            }
-        }
+    fn kill_job(&mut self, id: usize) {
+        todo!()
     }
 
-    pub fn on_line_service(&mut self, line_number: i64, mut num_processed_items: i32) {
-        let line = self.lines.get_mut(&line_number).unwrap();
-        while num_processed_items > 0 && !line.customers.is_empty() {
-            let customer = if let Some(customer) = line.customers.front_mut().unwrap() {
-                customer
-            } else {
-                line.customers.pop_front();
-                continue;
-            };
-            let item_num = min(num_processed_items, customer.num_items);
-            num_processed_items -= item_num;
-            customer.num_items -= item_num;
-            customer.processed_items += item_num;
-            if customer.num_items == 0 {
-                let customer_id = customer.id;
-                line.customers.pop_front();
-                self.customers_line_pos.remove(&customer_id);
-                on_customer_exit(customer_id);
-            }
-        }
-    }
-
-    pub fn on_lines_service(&mut self) {
-        let mut line_numbers: Vec<i64> = self.lines.keys().cloned().collect();
-        line_numbers.sort();
-        for line_number in line_numbers {
-            self.on_line_service(line_number, 1);
-        }
+    fn tick(&mut self) -> usize {
+        todo!()
     }
 }
 
-fn main() {
-    let stdin = io::stdin();
-    let mut stdin_iterator = stdin.lock().lines();
-
-    let mut checkout_tracker: SupermarketCheckout = SupermarketCheckout::new();
-
-    let n = stdin_iterator
-        .next()
-        .unwrap()
-        .unwrap()
-        .trim()
-        .parse::<i64>()
-        .unwrap();
-
-    for _ in 0..n {
-        let line = stdin_iterator.next().unwrap().unwrap();
-        let parameters: Vec<&str> = line.split_whitespace().collect();
-
-        match parameters[0].trim() {
-            "CustomerEnter" => {
-                let customer_id = parameters[1].parse::<i64>().unwrap();
-                let line_number = parameters[2].parse::<i64>().unwrap();
-                let num_items = parameters[3].parse::<i32>().unwrap();
-                checkout_tracker.on_customer_enter(customer_id, line_number, num_items);
-            }
-            "BasketChange" => {
-                let customer_id = parameters[1].parse::<i64>().unwrap();
-                let new_num_items = parameters[2].parse::<i32>().unwrap();
-                checkout_tracker.on_basket_change(customer_id, new_num_items);
-            }
-            "LineService" => {
-                let line_number = parameters[1].parse::<i64>().unwrap();
-                let num_processed_items = parameters[2].parse::<i32>().unwrap();
-                checkout_tracker.on_line_service(line_number, num_processed_items);
-            }
-            "LinesService" => {
-                checkout_tracker.on_lines_service();
-            }
-            _ => {
-                panic!("Malformed input!");
-            }
-        };
-    }
-}
+fn main() {}

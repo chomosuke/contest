54dbd8968b7fb88a391c27b695e264f0a1ec51d6
chomosuke <a13323600@gmail.com>
2024-04-04 02:30:18 +1100

    atlassian OA 1

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,114 +1,108 @@
-#include <algorithm>     // IWYU pragma: keep
-#include <bitset>        // IWYU pragma: keep
-#include <cmath>         // IWYU pragma: keep
-#include <cstdint>       // IWYU pragma: keep
-#include <iostream>      // IWYU pragma: keep
-#include <map>           // IWYU pragma: keep
-#include <queue>         // IWYU pragma: keep
-#include <set>           // IWYU pragma: keep
-#include <string>        // IWYU pragma: keep
-#include <string_view>   // IWYU pragma: keep
-#include <tuple>         // IWYU pragma: keep
-#include <unordered_map> // IWYU pragma: keep
-#include <unordered_set> // IWYU pragma: keep
-#include <vector>        // IWYU pragma: keep
+#include <bits/stdc++.h>
 
-namespace hash_tuple {
-template <typename TT> struct hash {
-    size_t operator()(TT const &tt) const { return std::hash<TT>()(tt); }
-};
-namespace {
+using namespace std;
 
-template <class T> inline void hash_combine(std::size_t &seed, T const &v) {
-    seed ^= hash_tuple::hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'romanizer' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts INTEGER_ARRAY numbers as parameter.
+ */
+
+vector<string> romanizer(vector<int> numbers) {
+    vector<string> result;
+    for (auto number : numbers) {
+        const int max_digit = 4;
+        int digits[max_digit] = {
+            number % 10,
+            number % 100 / 10,
+            number % 1000 / 100,
+            number / 1000,
+        };
+        char presentation[max_digit][2] = {
+            {'I', 'V'},
+            {'X', 'L'},
+            {'C', 'D'},
+            {'M', 'M'},
+        };
+        stringstream roman;
+        for (int i = max_digit - 1; i >= 0; i--) {
+            if (digits[i] <= 3) {
+                for (int j = 0; j < digits[i]; j++) {
+                    roman << presentation[i][0];
+                }
+            } else if (digits[i] == 4) {
+                roman << presentation[i][0];
+                roman << presentation[i][1];
+            } else if (digits[i] < 9) {
+                roman << presentation[i][1];
+                for (int j = 0; j < digits[i] - 5; j++) {
+                    roman << presentation[i][0];
+                }
+            } else {
+                roman << presentation[i][0];
+                roman << presentation[i + 1][0];
+            }
+        }
+        result.push_back(roman.str());
+    }
+    return result;
 }
 
-template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
-struct HashValueImpl {
-    static void apply(size_t &seed, Tuple const &tuple) {
-        HashValueImpl<Tuple, Index - 1>::apply(seed, tuple);
-        hash_combine(seed, std::get<Index>(tuple));
-    }
-};
-
-template <class Tuple> struct HashValueImpl<Tuple, 0> {
-    static void apply(size_t &seed, Tuple const &tuple) {
-        hash_combine(seed, std::get<0>(tuple));
-    }
-};
-} // namespace
-
-template <typename... TT> struct hash<std::tuple<TT...>> {
-    size_t operator()(std::tuple<TT...> const &tt) const {
-        size_t seed{0};
-        HashValueImpl<std::tuple<TT...>>::apply(seed, tt);
-        return seed;
-    }
-};
-} // namespace hash_tuple
-
-// unordered_map<tuple<Int, Float>, Int, hash_tuple::hash<tuple<Int, Float>>>
-//     memoize;
-
-// typedef int64_t Int;
-// [[maybe_unused]] const Int Int_max = INT64_MAX;
-// [[maybe_unused]] const Int Int_min = INT64_MIN;
-// typedef long double Float;
-
 int main() {
-    int test_cases_count{};
-    std::cin >> test_cases_count;
-    for (int test_case = 0; test_case < test_cases_count; test_case++) {
-        int n{};
-        int k{};
-        std::cin >> n;
-        std::cin >> k;
-        std::vector<int> arr{};
-        for (int i = 0; i < n; i++) {
-            int a{};
-            std::cin >> a;
-            arr.push_back(a);
-        }
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-        std::vector<long long> prefix_sum{};
-        prefix_sum.push_back(0);
-        for (int i = 0; i < arr.size(); i++) {
-            long long next_sum = prefix_sum.back() + arr[i];
-            prefix_sum.push_back(next_sum);
-        }
+    string numbers_count_temp;
+    getline(cin, numbers_count_temp);
 
-        std::vector<long long> min_til{};
-        min_til.push_back(prefix_sum[0]);
-        for (int i = 1; i < prefix_sum.size(); i++) {
-            long long next_min = std::min(min_til.back(), prefix_sum[i]);
-            min_til.push_back(next_min);
-        }
+    int numbers_count = stoi(ltrim(rtrim(numbers_count_temp)));
 
-        std::vector<long long> max_bef{};
-        max_bef.push_back(prefix_sum.back());
-        for (int i = prefix_sum.size() - 2; i >= 0; i--) {
-            long long next_max = std::max(max_bef.back(), prefix_sum[i]);
-            max_bef.push_back(next_max);
-        }
-        std::reverse(max_bef.begin(), max_bef.end());
+    vector<int> numbers(numbers_count);
 
-        long long max_diff = 0;
-        for (int i = 0; i + 1 < max_bef.size(); i++) {
-            max_diff = std::max(max_diff, max_bef[i + 1] - min_til[i]);
-        }
+    for (int i = 0; i < numbers_count; i++) {
+        string numbers_item_temp;
+        getline(cin, numbers_item_temp);
 
-        const long long m = 1000000000 + 7;
-        long long res = max_diff;
-        for (int i = 0; i < k; i++) {
-            res *= 2;
-            res %= m;
-        }
-        res += prefix_sum.back() - max_diff;
-        res %= m;
-        if (res < 0) {
-            res += m;
-        }
+        int numbers_item = stoi(ltrim(rtrim(numbers_item_temp)));
 
-        std::cout << res << std::endl;
+        numbers[i] = numbers_item;
     }
+
+    vector<string> result = romanizer(numbers);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(s.begin(),
+            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
 }

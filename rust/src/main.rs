d2650d8f066e6e73e8b1a42a7021ff9243248dc4
chomosuke <a13323600@gmail.com>
2024-04-06 07:51:57 +1100

    solution for partition for beginner contest

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,28 +1,174 @@
-#[allow(unused_imports)]
+#![allow(unused_imports, dead_code, clippy::needless_range_loop, unused_labels)]
+use io::*;
 use std::{
-    cmp::min,
-    collections::{HashMap, VecDeque},
-    io::{self, BufRead},
+    cmp::{max, min, Ordering},
+    collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
+    fs,
+    io::{stdin, stdout, BufReader},
+    iter,
+    mem::{self, swap},
+    usize,
 };
 
-struct Scheduler {}
-
-impl Scheduler {
-    fn new() -> Self {
-        todo!()
+fn main() {
+    let mut sc = Scanner::new(stdin());
+    let mut pt = Printer::new(stdout());
+    let n = sc.next::<u64>();
+    let mut sum = 0;
+    for i in 1..=n {
+        sum += i;
     }
-
-    fn new_job(&mut self, id: usize, steps: impl Iterator) {
-        todo!()
+    for i in 2..n {
+        if sum % i == 0 {
+            pt.println("Yes");
+            pt.println(i);
+            pt.print_iter((1..=n).filter(|&a| a != i));
+            return;
+        }
     }
-
-    fn kill_job(&mut self, id: usize) {
-        todo!()
-    }
-
-    fn tick(&mut self) -> usize {
-        todo!()
-    }
+    pt.println("No");
 }
 
-fn main() {}
+mod io {
+    use std::collections::{HashSet, VecDeque};
+    use std::fmt::Display;
+    use std::io::{BufReader, BufWriter, Lines, Read, Write};
+    use std::marker::PhantomData;
+    use std::{any::type_name, io::BufRead, str::FromStr};
+
+    pub struct ScannerIter<'a, R: Read, T> {
+        remaining: usize,
+        sc: &'a mut Scanner<R>,
+        item: PhantomData<T>,
+    }
+
+    impl<R: Read, T: FromStr> Iterator for ScannerIter<'_, R, T> {
+        type Item = T;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.remaining == 0 {
+                None
+            } else {
+                self.remaining -= 1;
+                Some(self.sc.next::<T>())
+            }
+        }
+    }
+
+    pub struct Scanner<R: Read> {
+        tokens: VecDeque<String>,
+        delimiters: Option<HashSet<char>>,
+        lines: Lines<BufReader<R>>,
+    }
+    impl<R: Read> Scanner<R> {
+        pub fn new(source: R) -> Self {
+            Self {
+                tokens: VecDeque::new(),
+                delimiters: None,
+                lines: BufReader::new(source).lines(),
+            }
+        }
+
+        pub fn with_delimiters(source: R, delimiters: &[char]) -> Self {
+            Self {
+                tokens: VecDeque::new(),
+                delimiters: Some(delimiters.iter().copied().collect()),
+                lines: BufReader::new(source).lines(),
+            }
+        }
+
+        pub fn next<T: FromStr>(&mut self) -> T {
+            let token = loop {
+                let front = self.tokens.pop_front();
+                if let Some(token) = front {
+                    break token;
+                }
+                self.receive_input();
+            };
+            token
+                .parse::<T>()
+                .unwrap_or_else(|_| panic!("input {} isn't a {}", token, type_name::<T>()))
+        }
+
+        pub fn next_n<T: FromStr>(&mut self, n: usize) -> ScannerIter<'_, R, T> {
+            ScannerIter {
+                remaining: n,
+                sc: self,
+                item: PhantomData,
+            }
+        }
+
+        pub fn next_line(&mut self) -> String {
+            assert!(self.tokens.is_empty(), "You have unprocessed token");
+            self.lines
+                .next()
+                .and_then(|e| e.ok())
+                .expect("Failed to read.")
+        }
+
+        fn receive_input(&mut self) {
+            let line = self
+                .lines
+                .next()
+                .and_then(|e| e.ok())
+                .expect("Failed to read.");
+            if let Some(delimiters) = &self.delimiters {
+                for token in line.split(|c| delimiters.contains(&c)) {
+                    self.tokens.push_back(token.to_string());
+                }
+            } else {
+                for token in line.split_whitespace() {
+                    self.tokens.push_back(token.to_string());
+                }
+            }
+        }
+    }
+
+    pub struct Printer<W: Write> {
+        writer: BufWriter<W>,
+    }
+    impl<W: Write> Printer<W> {
+        pub fn new(destination: W) -> Self {
+            Self {
+                writer: BufWriter::new(destination),
+            }
+        }
+
+        pub fn print(&mut self, s: impl Display) {
+            self.writer
+                .write_all(s.to_string().as_bytes())
+                .expect("print failed.");
+        }
+
+        pub fn print_bytes(&mut self, b: &[u8]) {
+            self.writer.write_all(b).expect("print_bytes failed.");
+        }
+
+        pub fn println(&mut self, s: impl Display) {
+            self.print(s);
+            self.newline();
+        }
+
+        pub fn newline(&mut self) {
+            self.print_bytes(&[b'\n']);
+        }
+
+        pub fn print_iter(&mut self, mut iter: impl Iterator<Item = impl Display>) {
+            if let Some(e) = iter.next() {
+                self.print(&e);
+                for e in iter {
+                    self.print_bytes(&[b' ']);
+                    self.print(&e);
+                }
+            }
+            self.newline();
+        }
+    }
+    impl<W: Write> Drop for Printer<W> {
+        fn drop(&mut self) {
+            self.writer
+                .flush()
+                .expect("flush failed when dropping Printer.");
+        }
+    }
+}

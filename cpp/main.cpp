9b2d024a3240b8bd3992c81490b893fef929b5a3
chomosuke <a13323600@gmail.com>
2024-04-17 17:08:38 +1000

    1294C

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,74 +1,46 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-
-/*
- * Complete the 'findLowestStartingStair' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY jumps as parameter.
- */
-
-int findLowestStartingStair(vector<int> jumps) {
-    int case_{1};
-    int min_case{1};
-    for (auto jump : jumps) {
-        case_ += jump;
-        min_case = min(min_case, case_);
-    }
-    if (min_case < 1) {
-        return 1 - min_case + 1;
-    } else {
-        return 1;
-    }
-}
+#include <algorithm>     // IWYU pragma: keep
+#include <bitset>        // IWYU pragma: keep
+#include <cmath>         // IWYU pragma: keep
+#include <cstdint>       // IWYU pragma: keep
+#include <iostream>      // IWYU pragma: keep
+#include <map>           // IWYU pragma: keep
+#include <queue>         // IWYU pragma: keep
+#include <set>           // IWYU pragma: keep
+#include <string>        // IWYU pragma: keep
+#include <string_view>   // IWYU pragma: keep
+#include <tuple>         // IWYU pragma: keep
+#include <unordered_map> // IWYU pragma: keep
+#include <unordered_set> // IWYU pragma: keep
+#include <vector>        // IWYU pragma: keep
 
 int main() {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string jumps_count_temp;
-    getline(cin, jumps_count_temp);
-
-    int jumps_count = stoi(ltrim(rtrim(jumps_count_temp)));
-
-    vector<int> jumps(jumps_count);
-
-    for (int i = 0; i < jumps_count; i++) {
-        string jumps_item_temp;
-        getline(cin, jumps_item_temp);
-
-        int jumps_item = stoi(ltrim(rtrim(jumps_item_temp)));
-
-        jumps[i] = jumps_item;
+    int test_count{};
+    std::cin >> test_count;
+    for (int test_case = 0; test_case < test_count; test_case++) {
+        int n{};
+        std::cin >> n;
+        int a{2};
+        for (; a * a * a < n; a++) {
+            if (n % a == 0) {
+                break;
+            }
+        }
+        if (n % a != 0) {
+            std::cout << "NO" << std::endl;
+            continue;
+        }
+        n /= a;
+        int b{a + 1};
+        for (; b * b < n; b++) {
+            if (n % b == 0) {
+                break;
+            }
+        }
+        if (b * b < n && n % b == 0) {
+            std::cout << "YES" << std::endl;
+            std::cout << a << " " << b << " " << n / b << std::endl;
+        } else {
+            std::cout << "NO" << std::endl;
+        }
     }
-
-    int result = findLowestStartingStair(jumps);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(s.begin(),
-            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end());
-
-    return s;
 }

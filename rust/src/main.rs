9aa32c4dd89a9efa01a7ae16e094627da118312d
chomosuke <a13323600@gmail.com>
2024-05-13 16:18:09 +1000

    1556D

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,43 +10,74 @@ use std::{
     usize,
 };
 
+type N = u32;
+
+fn get_nth_bit(a: N, n: usize) -> bool {
+    (a >> n) % 2 == 1
+}
+
+fn set_nth_bit(a: &mut N, n: usize, b: bool) {
+    if b {
+        *a = *a | (1 << n)
+    } else {
+        *a = *a & !(1 << n)
+    }
+}
+
 fn main() {
     let mut sc = Scanner::new(stdin());
-    let mut pt = Printer::new(stdout());
-    let a = sc.next::<u64>();
-    let b = sc.next::<u64>();
-    let (a, b) = (max(a, b), min(a, b));
-    let d = a - b;
-    if d == 0 {
-        pt.println(0);
-    } else if b >= d {
-        let k = d - a % d;
-        if k == d {
-            pt.println(0);
+    // let mut pt = Printer::new(stdout());
+    let n = sc.next::<usize>();
+    let k = sc.next::<usize>();
+    println!("and 1 2");
+    let and12 = sc.next::<u32>();
+    println!("or 1 2");
+    let or12 = sc.next::<u32>();
+    println!("and 1 3");
+    let and13 = sc.next::<u32>();
+    println!("or 1 3");
+    let or13 = sc.next::<u32>();
+    println!("and 2 3");
+    let and23 = sc.next::<u32>();
+    println!("or 2 3");
+    let or23 = sc.next::<u32>();
+    let mut v = vec![0; n];
+    for i in 0..32 {
+        let and12 = get_nth_bit(and12, i);
+        let or12 = get_nth_bit(or12, i);
+        let and13 = get_nth_bit(and13, i);
+        let or13 = get_nth_bit(or13, i);
+        let and23 = get_nth_bit(and23, i);
+        let or23 = get_nth_bit(or23, i);
+        if and12 == or12 {
+            set_nth_bit(&mut v[0], i, and12);
+            set_nth_bit(&mut v[1], i, and12);
+            set_nth_bit(&mut v[2], i, (and23 != or23) != and12);
+        } else if and13 == or13 {
+            set_nth_bit(&mut v[0], i, and13);
+            set_nth_bit(&mut v[2], i, and13);
+            set_nth_bit(&mut v[1], i, !and13);
         } else {
-            pt.println(k);
-        }
-    } else {
-        let mut factors = Vec::new();
-        let mut i = 1;
-        while i * i < d {
-            if d % i == 0 {
-                factors.push(i);
-                factors.push(d / i);
-            }
-            i += 1;
-        }
-        if i * i == d {
-            factors.push(i);
-        }
-        factors.sort_unstable();
-        for f in factors {
-            if f >= b {
-                pt.println(f - b);
-                break;
-            }
+            set_nth_bit(&mut v[1], i, and23);
+            set_nth_bit(&mut v[2], i, and23);
+            set_nth_bit(&mut v[0], i, !and23);
         }
     }
+    for i in 3..n {
+        let v1 = v[0];
+        println!("and 1 {}", i + 1);
+        let and1i = sc.next::<u32>();
+        println!("or 1 {}", i + 1);
+        let or1i = sc.next::<u32>();
+        for j in 0..32 {
+            let and1i = get_nth_bit(and1i, j);
+            let or1i = get_nth_bit(or1i, j);
+            let v1 = get_nth_bit(v1, j);
+            set_nth_bit(&mut v[i], j, (and1i != or1i) != v1);
+        }
+    }
+    v.sort_unstable();
+    println!("finish {}", v[k - 1])
 }
 
 mod io {

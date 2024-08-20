83ec00ffa84d4eb0909b3836025327d57ca88acd
chomosuke <a13323600@gmail.com>
2024-08-21 02:32:56 +1000

    2001D

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -26,44 +26,243 @@ use std::{
 type I = i128;
 type U = u128;
 
-fn query(sc: &mut Scanner<Stdin>, a: usize, b: usize) -> usize {
-    println!("? {} {}", a + 1, b + 1);
-    sc.next::<usize>() - 1
-}
+mod seg {
+    use std::ops::{Bound::*, Index, IndexMut};
+    use std::ops::{Deref, RangeBounds};
 
-fn ans(edges: &Vec<(usize, usize)>) {
-    print!("!");
-    for (v, u) in edges {
-        print!(" {} {}", v + 1, u + 1);
-    }
-    println!();
-}
-
-fn solve(sc: &mut Scanner<Stdin>, _pt: &mut Printer<Stdout>) {
-    let n = sc.next::<usize>();
-    let mut edges = Vec::with_capacity(n);
-    let mut unknown = BTreeSet::new();
-    for i in 1..n {
-        unknown.insert(i);
-    }
-    while let Some(&(mut b)) = unknown.last() {
-        // get an edge
-        let mut a = 0;
-        loop {
-            let x = query(sc, a, b);
-            if x == a {
-                unknown.remove(&b);
-                edges.push((a, b));
-                break;
-            }
-            if unknown.contains(&x) {
-                b = x;
-            } else {
-                a = x;
-            }
+    const USIZE_BITS: u32 = 64;
+    fn log2_ceil(x: usize) -> u32 {
+        if x == 0 {
+            0
+        } else {
+            USIZE_BITS - (x - 1).leading_zeros()
         }
     }
-    ans(&edges);
+    fn pow2_ceil(x: usize) -> usize {
+        let n = log2_ceil(x);
+        2usize.pow(n)
+    }
+
+    pub struct SegmentTree<E, F> {
+        combine: F,
+        inner: Vec<E>,
+        tree: Vec<E>,
+        inner_cap: usize,
+        zero: E,
+    }
+    impl<E, F> Deref for SegmentTree<E, F> {
+        type Target = Vec<E>;
+
+        fn deref(&self) -> &Self::Target {
+            &self.inner
+        }
+    }
+    impl<E: Clone, F: Fn(&E, &E) -> E> SegmentTree<E, F> {
+        fn parent(i: usize) -> usize {
+            i / 2
+        }
+        fn left(i: usize) -> usize {
+            2 * i
+        }
+        fn right(i: usize) -> usize {
+            2 * i + 1
+        }
+
+        pub fn new(zero: E, combine: F) -> Self {
+            Self {
+                combine,
+                inner: Vec::new(),
+                tree: Vec::new(),
+                inner_cap: 0,
+                zero,
+            }
+        }
+        pub fn with_capacity(capacity: usize, zero: E, combine: F) -> Self {
+            Self {
+                combine,
+                inner: Vec::with_capacity(capacity),
+                tree: Vec::with_capacity(pow2_ceil(capacity) * 2),
+                inner_cap: 0,
+                zero,
+            }
+        }
+        /// O(n)
+        pub fn from_vec(vec: Vec<E>, zero: E, combine: F) -> Self {
+            let mut iv = Self {
+                combine,
+                inner: vec,
+                zero,
+                tree: Vec::new(),
+                inner_cap: 0,
+            };
+            iv.rebuild();
+            iv
+        }
+
+        /// O(n)
+        fn rebuild(&mut self) {
+            let inner = &mut self.inner;
+            let combine = &self.combine;
+            let zero = &self.zero;
+            let inner_cap = pow2_ceil(inner.len());
+            let mut tree = vec![zero.clone(); inner_cap * 2];
+            tree[inner_cap..(inner_cap + inner.len())].clone_from_slice(&inner[..]);
+            let mut n = inner_cap;
+            while n > 1 {
+                n /= 2;
+                for i in n..(n * 2) {
+                    tree[i] = combine(&tree[Self::left(i)], &tree[Self::right(i)]);
+                }
+            }
+            self.tree = tree;
+            self.inner_cap = inner_cap;
+        }
+
+        /// O(log(n))
+        pub fn query(&self, rng: impl RangeBounds<usize>) -> E {
+            let start = match rng.start_bound() {
+                Excluded(x) => x + 1,
+                Included(x) => *x,
+                Unbounded => 0,
+            };
+            let end = match rng.end_bound() {
+                Excluded(x) => x - 1,
+                Included(x) => *x,
+                Unbounded => self.inner.len() - 1,
+            };
+            let mut start = start + self.inner_cap;
+            let mut end = end + self.inner_cap;
+            let mut result = self.zero.clone();
+            while start <= end {
+                if start % 2 == 1 {
+                    result = (self.combine)(&result, &self.tree[start]);
+                    start += 1;
+                }
+                if end % 2 == 0 {
+                    result = (self.combine)(&result, &self.tree[end]);
+                    end -= 1;
+                }
+                start = Self::parent(start);
+                end = Self::parent(end);
+            }
+            result
+        }
+
+        fn update(&mut self, index: usize) {
+            self.tree[index + self.inner_cap] = if index < self.inner.len() {
+                self.inner[index].clone()
+            } else {
+                self.zero.clone()
+            };
+            let mut index = index + self.inner_cap;
+            while index > 1 {
+                index = Self::parent(index);
+                self.tree[index] = (self.combine)(
+                    &self.tree[Self::left(index)],
+                    &self.tree[Self::right(index)],
+                );
+            }
+        }
+        /// O(log(n))
+        pub fn push(&mut self, e: E) {
+            self.inner.push(e);
+            if self.inner.len() > self.inner_cap {
+                self.rebuild();
+            } else {
+                self.update(self.inner.len() - 1);
+            }
+        }
+        /// O(log(n))
+        pub fn pop(&mut self) -> Option<E> {
+            let e = self.inner.pop();
+            self.update(self.inner.len());
+            e
+        }
+        /// O(log(n))
+        pub fn set(&mut self, i: usize, e: E) {
+            self.inner[i] = e;
+            self.update(i);
+        }
+    }
+}
+use seg::*;
+
+/// Will look for first i such that p(i) == false.
+fn search(start: usize, step: usize, p: impl Fn(usize) -> bool) -> usize {
+    assert!(p(start));
+    let mut index = start;
+    let mut step = step;
+    while step > 0 {
+        if p(index + step) {
+            index += step;
+        } else {
+            step /= 2;
+        }
+    }
+    index + 1
+}
+
+fn solve(sc: &mut Scanner<Stdin>, pt: &mut Printer<Stdout>) {
+    let n = sc.next::<usize>();
+    let arr = sc.next_n::<U>(n);
+    let mut unique = HashSet::new();
+    let mut last_unique = vec![0; n];
+    for (i, &a) in arr.iter().enumerate().rev() {
+        if !unique.contains(&a) {
+            unique.insert(a);
+            last_unique[i] = 1;
+        }
+    }
+
+    let mut last_unique_count = SegmentTree::from_vec(last_unique, 0, |&a, &b| a + b);
+
+    let mut sg_max = SegmentTree::from_vec(arr.clone(), U::MIN, |&a, &b| max(a, b));
+    let mut sg_min = SegmentTree::from_vec(arr.clone(), U::MAX, |&a, &b| min(a, b));
+
+    let mut atois = HashMap::<U, Vec<usize>>::new();
+    for (i, &a) in arr.iter().enumerate() {
+        atois.entry(a).or_default().push(i);
+    }
+
+    let unique_count = last_unique_count.query(0..n);
+
+    let mut seq = Vec::with_capacity(unique_count);
+    let mut left = 0;
+    for i in 0..unique_count {
+        let right = {
+            let last_unique_from_right = unique_count - i - 1;
+            search(left, n / 2, |right| {
+                if right > n {
+                    return false;
+                }
+                last_unique_count.query(right..n) > last_unique_from_right
+            })
+        };
+
+        let best = if seq.len() % 2 == 0 {
+            sg_max.query(left..right)
+        } else {
+            sg_min.query(left..right)
+        };
+        seq.push(best);
+
+        for &i in &atois[&best] {
+            sg_min.set(i, U::MAX);
+            sg_max.set(i, U::MIN);
+
+            last_unique_count.set(i, 0);
+        }
+
+        // update marks
+
+        while arr[left] != best {
+            left += 1;
+        }
+        left += 1;
+    }
+
+    pt.println(seq.len());
+    pt.print_iter(seq.iter());
 }
 
 fn main() {

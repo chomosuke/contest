6eb2f3ae664c8bd116ef0b1a8a23d895b552c033
chomosuke <a13323600@gmail.com>
2024-12-14 16:17:03 +1100

    AoC D14 1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -24,150 +24,6 @@ use std::{
     },
 };
 
-type Int = i128;
-
-fn find_linear_comb(a: Int, b: Int) -> (Int, Int, Int) {
-    let mut x_a1 = 1;
-    let mut y_a1 = 0;
-    let mut x_b1 = 0;
-    let mut y_b1 = 1;
-    let mut a1 = a;
-    let mut b1 = b;
-    while b1 != 0 {
-        let q = a1 / b1;
-        let a2 = b1;
-        let b2 = a1 - q * b1;
-        let x_a2 = x_b1;
-        let y_a2 = y_b1;
-        let x_b2 = x_a1 - q * x_b1;
-        let y_b2 = y_a1 - q * y_b1;
-
-        a1 = a2;
-        b1 = b2;
-        x_a1 = x_a2;
-        y_a1 = y_a2;
-        x_b1 = x_b2;
-        y_b1 = y_b2;
-    }
-    (a1, x_a1, y_a1)
-}
-
-struct LDESIter {
-    xy: (Int, Int),
-    diff: (Int, Int),
-    k_range: Range<Int>,
-}
-impl Iterator for LDESIter {
-    type Item = (Int, Int);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let k = self.k_range.next()?;
-        Some((self.xy.0 + k * self.diff.0, self.xy.1 + k * self.diff.1))
-    }
-}
-
-fn linear_diophantine_eq_sols(
-    a: Int,
-    b: Int,
-    c: Int,
-    x_range: Range<Int>,
-    y_range: Range<Int>,
-) -> LDESIter {
-    let (gcd, x, y) = find_linear_comb(a, b);
-    if c % gcd != 0 {
-        return LDESIter {
-            xy: (0, 0),
-            diff: (0, 0),
-            k_range: 0..0,
-        };
-    }
-    let n = c / gcd;
-    let x = x * n;
-    let y = y * n;
-
-    let mut x_diff = b / gcd;
-    let mut y_diff = -a / gcd;
-    if x_diff < 0 {
-        x_diff = -x_diff;
-        y_diff = -y_diff;
-    }
-
-    let x_end = x_range.end - 1;
-    let x_start = x_range.start;
-    let y_end = y_range.end - 1;
-    let y_start = y_range.start;
-
-    fn div_ceil(x: Int, y: Int) -> Int {
-        if (x < 0) != (y < 0) {
-            x / y
-        } else {
-            (x - 1) / y + 1
-        }
-    }
-
-    fn div_floor(x: Int, y: Int) -> Int {
-        if (x < 0) != (y < 0) {
-            (x + 1) / y - 1
-        } else {
-            x / y
-        }
-    }
-
-    let mut k_start = Int::MIN;
-    let mut k_end = Int::MAX;
-    if x_diff == 0 {
-        if x_start <= x && x < x_end {
-            return LDESIter {
-                xy: (x, 0),
-                diff: (0, 1),
-                k_range: y_start..(y_end + 1),
-            };
-        } else {
-            return LDESIter {
-                xy: (0, 0),
-                diff: (0, 0),
-                k_range: 0..0,
-            };
-        }
-    }
-    if x_diff < 0 {
-        k_start = k_start.max(div_ceil(x_end - x, x_diff));
-        k_end = k_end.min(div_floor(x_start - x, x_diff));
-    } else {
-        k_start = k_start.max(div_ceil(x_start - x, x_diff));
-        k_end = k_end.min(div_floor(x_end - x, x_diff));
-    }
-
-    if y_diff == 0 {
-        if y_start <= y && y < y_end {
-            return LDESIter {
-                xy: (0, y),
-                diff: (1, 0),
-                k_range: x_start..(x_end + 1),
-            };
-        } else {
-            return LDESIter {
-                xy: (0, 0),
-                diff: (0, 0),
-                k_range: 0..0,
-            };
-        }
-    }
-    if y_diff < 0 {
-        k_start = k_start.max(div_ceil(y_end - y, y_diff));
-        k_end = k_end.min(div_floor(y_start - y, y_diff));
-    } else {
-        k_start = k_start.max(div_ceil(y_start - y, y_diff));
-        k_end = k_end.min(div_floor(y_end - y, y_diff));
-    }
-
-    LDESIter {
-        xy: (x, y),
-        diff: (x_diff, y_diff),
-        k_range: k_start..(k_end + 1),
-    }
-}
-
 mod input;
 use input::*;
 
@@ -179,63 +35,55 @@ struct XY {
     y: I,
 }
 
-struct Machine {
-    a: XY,
-    b: XY,
+struct Robot {
     p: XY,
+    v: XY,
 }
 
 fn main() {
-    let re = Regex::new(
-        r"Button A: X\+(?<AX>\d+), Y\+(?<AY>\d+)
-Button B: X\+(?<BX>\d+), Y\+(?<BY>\d+)
-Prize: X=(?<PX>\d+), Y=(?<PY>\d+)",
-    )
-    .unwrap();
+    let re = Regex::new(r"p=(?<px>-?\d+),(?<py>-?\d+) v=(?<vx>-?\d+),(?<vy>-?\d+)").unwrap();
 
-    let machines = re.captures_iter(INPUT).map(|cap| Machine {
-        a: XY {
-            x: cap.name("AX").unwrap().as_str().parse().unwrap(),
-            y: cap.name("AY").unwrap().as_str().parse().unwrap(),
-        },
-        b: XY {
-            x: cap.name("BX").unwrap().as_str().parse().unwrap(),
-            y: cap.name("BY").unwrap().as_str().parse().unwrap(),
-        },
-        p: XY {
-            x: cap.name("PX").unwrap().as_str().parse::<I>().unwrap() + 10000000000000,
-            y: cap.name("PY").unwrap().as_str().parse::<I>().unwrap() + 10000000000000,
-        },
-    });
+    let mut robots = re
+        .captures_iter(INPUT)
+        .map(|cap| Robot {
+            p: XY {
+                x: cap.name("px").unwrap().as_str().parse::<I>().unwrap(),
+                y: cap.name("py").unwrap().as_str().parse::<I>().unwrap(),
+            },
+            v: XY {
+                x: cap.name("vx").unwrap().as_str().parse::<I>().unwrap(),
+                y: cap.name("vy").unwrap().as_str().parse::<I>().unwrap(),
+            },
+        })
+        .collect::<Vec<_>>();
 
-    let mut tokens = 0;
+    let width = 101;
+    let height = 103;
 
-    for Machine { a, b, p } in machines {
-        let bd = b.y * a.x - b.x * a.y;
-        if bd == 0 {
-            continue;
-        }
-        let bn = p.y * a.x - p.x * a.y;
-        if bn % bd != 0 {
-            continue;
-        }
-        let be = bn / bd;
+    for r in &mut robots {
+        r.p.x += r.v.x * 100;
+        r.p.x = r.p.x.rem_euclid(width);
+        r.p.y += r.v.y * 100;
+        r.p.y = r.p.y.rem_euclid(height);
+    }
 
-        let ad = a.x;
-        if ad == 0 {
-            continue;
+    let mut quats = (0_usize, 0_usize, 0_usize, 0_usize);
+    for Robot { p, .. } in &robots {
+        if p.x < width / 2 && p.y < height / 2 {
+            quats.0 += 1;
         }
-        let an = p.x - be * b.x;
-        if an % ad != 0 {
-            continue;
+        if p.x > width / 2 && p.y < height / 2 {
+            quats.1 += 1;
         }
-        let ae = an / a.x;
-        if ae >= 0 && be >= 0 {
-            tokens += ae * 3 + be;
+        if p.x > width / 2 && p.y > height / 2 {
+            quats.2 += 1;
+        }
+        if p.x < width / 2 && p.y > height / 2 {
+            quats.3 += 1;
         }
     }
 
-    println!("{tokens}");
+    println!("{}", quats.0 * quats.1 * quats.2 * quats.3);
 }
 
 mod io {
